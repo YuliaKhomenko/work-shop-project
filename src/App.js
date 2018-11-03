4a15a1ef5f7ef3adb9c901ab37be12e8b93ec87a
Yulia <homka15@i.ua>
2018-11-03 13:23:06 +0200

    add .css files, image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import React from 'react'
-
+import './reset.css'
+import './base.css'
+import './header.css'
+import './menu.css'
+import './cart.css'
 
 export const App = () => {
 	return (
 		<div className="App">
-			Text
+			<header className="header">
+		<div className="container">
+			<div className="row">
+				<div className="col-lg-3">
+					<div className="logo">
+						<img src="img/logo.png" alt=""/>
+					</div>
+				</div>
+				<div className="col-lg-6">
+					<div className="menu">
+						<ul>
+							<li><a href="">Home</a></li>
+							<li><a href="">Product</a></li>
+							<li><a href="">Payment</a></li>
+							<li><a href="">Shipping</a></li>
+						</ul>
+					</div>
+				</div>
+				<div className="col-lg-3">
+					<div className="cart text-center">
+						<div className="products-count">0</div>
+						<div className="products-price">$500</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<main className="main">
+		<div className="container">
+			<div className="row">
+				<div className="col-lg-3">
+					
+				</div>
+				<div className="col-lg-9">
+					
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer className="footer">
+		
+	</footer>
 
 		</div>
 	)
