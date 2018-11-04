c60ecc461a254f61b16b80df32ce54c5f96ad960
Yulia <homka15@i.ua>
2018-11-04 12:21:00 +0200

    create folder Main and fie Main.js inside the folder

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import  Header from './Header/Header'
+import  Main from './Main/Main'
 import '../common/style/reset.css'
 import '../common/style/base.css'
 
@@ -17,22 +18,7 @@ const App = () => {
 
 
 
-const Main = () => {
-	return (
-		<main className="main">
-		<div className="container">
-			<div className="row">
-				<div className="col-lg-3">
-					
-				</div>
-				<div className="col-lg-9">
-					
-				</div>
-			</div>
-		</div>
-	</main>
-	)
-}
+
 
 const Footer = () => {
 	return (
