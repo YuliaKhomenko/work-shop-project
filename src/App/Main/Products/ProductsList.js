dab316811fef17b7b5f33df4d4e386db4af0e1b5
Yulia <homka15@i.ua>
2018-11-04 14:11:00 +0200

    add props with descriptions of the products

--- src/App/Main/Products/ProductsList.js
+++ src/App/Main/Products/ProductsList.js
@@ -9,16 +9,41 @@ const ProductsList = () => {
             <h2>Products List</h2>
                 <div className="row">
                     <div className="col-lg-6">
-                        <ProductsListItem />
+                        <ProductsListItem
+                            name="iPhone 8"
+                            description="This is iPhone 8"
+                            type="phone"
+                            capacity={64}
+                            price={499}
+                        />
                     </div>
                     <div className="col-lg-6">
-                        <ProductsListItem />
+                        <ProductsListItem
+                          name="iPhone 8 plus"
+                          description="This is iPhone 8 plus"
+                          type="phone"
+                          capacity={64}
+                          price={799}
+                        />
                     </div>
                     <div className="col-lg-6">
-                        <ProductsListItem />
+                        <ProductsListItem
+                          name="iPhone X"
+                          description="This is iPhone X"
+                          type="phone"
+                          capacity={256}
+                          price={999}
+                        />
                     </div>
                     <div className="col-lg-6">
-                        <ProductsListItem />
+                    <ProductsListItem
+                        name="iPhone XS"
+                            description="This is iPhone XS"
+                            type="phone"
+                            capacity={128}
+                            price={1199}
+                        
+                        />
                     </div>
                 </div>
         </div>
--- src/App/Main/Products/ProductsListItem.js
+++ src/App/Main/Products/ProductsListItem.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 import './ProductsListItem.css'
 
-const ProductsListItem = () => {
+const ProductsListItem = (props) => {
     return (
         <div className="products-list-item">
-            <div className="product-name">iPhone 7</div>
-            <div className="product-description">This is iPhone 7</div>
-            <div className="product-type">Type : phone</div>
-            <div className="product-capacity">Capacity : 64 Gb</div>
+            <div className="product-name">{props.name}</div>
+            <div className="product-description">{props.description}</div>
+            <div className="product-type">{props.type}</div>
+            <div className="product-capacity">{props.capacity}</div>
             <div className="product-quantity">
                 <button>-</button>
                 <input type="text"/>
                 <button>+</button>
             </div>
-            <div className="product-price">$ 499</div>
+            <div className="product-price">{props.price}</div>
             <button className="btn-add-to-cart">Add to cart</button>
         </div>
     )
