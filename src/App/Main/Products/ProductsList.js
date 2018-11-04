a84d36305f02a6ac0ee6a8728501eac75c4df15b
Yulia <homka15@i.ua>
2018-11-04 12:44:18 +0200

    create folder products and file productslist in Main folder

---
+++ src/App/Main/Products/ProductsList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductsList = () => {
+    return (
+        <div className="products-list">
+            <h2>Products List</h2>
+        </div>
+    )
+}
+
+
+export default ProductsList
