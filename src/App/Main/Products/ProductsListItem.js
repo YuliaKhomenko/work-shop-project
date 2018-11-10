b5826f72d0aed4cff4f41ca3c761968f54acd864
Yulia <homka15@i.ua>
2018-11-10 12:46:47 +0200

    add destructuring in ProductListItem, chenging props on its elements

--- src/App/Main/Products/ProductsListItem.js
+++ src/App/Main/Products/ProductsListItem.js
@@ -2,19 +2,25 @@ import React from 'react'
 
 import './ProductsListItem.css'
 
-const ProductsListItem = (props) => {
+const ProductsListItem = ({
+    name,
+    description,
+    type,
+    capacity,
+    price,
+}) => {
     return (
         <div className="products-list-item">
-            <div className="product-name">{props.name}</div>
-            <div className="product-description">{props.description}</div>
-            <div className="product-type">{props.type}</div>
-            <div className="product-capacity">{props.capacity} Gb</div>
+            <div className="product-name">{name}</div>
+            <div className="product-description">{description}</div>
+            <div className="product-type">{type}</div>
+            <div className="product-capacity">{capacity} Gb</div>
             <div className="product-quantity">
                 <button>-</button>
                 <input type="text"/>
                 <button>+</button>
             </div>
-            <div className="product-price">$ {props.price}</div>
+            <div className="product-price">$ {price}</div>
             <button className="btn-add-to-cart">Add to cart</button>
         </div>
     )
