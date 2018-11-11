83ca67338cd97dc149eec77819eb35bf98f9eee2
Yulia <homka15@i.ua>
2018-11-11 14:04:41 +0200

    add statefull method and add this.props for destructuring

--- src/App/Main/Products/ProductsListItem.js
+++ src/App/Main/Products/ProductsListItem.js
@@ -1,42 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import './ProductsListItem.css'
 
-const ProductsListItem = ({
-    image,
-    name,
-    description = "no decription...",
-    type,
-    capacity,
-    price,
-}) => {
-    return (
-        <div className="products-list-item">
-            <div className="product-image">
-                <img src={image} alt={name}/>
+class ProductsListItem extends Component {
+    render () {
+
+
+        return (
+            <div className="products-list-item">
+                <div className="product-image">
+                    <img src={this.props.image} alt={this.props.name}/>
+                </div>
+                <div className="product-name">{this.props.name}</div>
+                <div className="product-description">{this.props.description}</div>
+                <div className="product-type">Type: {this.props.type}</div>
+                <div className="product-capacity">Capacity: {this.props.capacity} Gb</div>
+                <div className="product-quantity">
+                    <button>-</button>
+                    <input type="text" value={1} readOnly/>
+                    <button>+</button>
+                </div>
+                <div className="product-price">$ {this.props.price}</div>
+                <button className="btn-add-to-cart">Add to cart</button>
             </div>
-            <div className="product-name">{name}</div>
-            <div className="product-description">{description}</div>
-            <div className="product-type">Type: {type}</div>
-            <div className="product-capacity">Capacity: {capacity} Gb</div>
-            <div className="product-quantity">
-                <button>-</button>
-                <input type="text"/>
-                <button>+</button>
-            </div>
-            <div className="product-price">$ {price}</div>
-            <button className="btn-add-to-cart">Add to cart</button>
-        </div>
-    )
+        )
+    }
 }
 
-ProductsListItem.propTypes = {
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    type: PropTypes.string,
-    capacity: PropTypes.number,
-    price: PropTypes.number.isRequired,
-}
+
+
+
+
+//ProductsListItem.propTypes = {
+  //  name: PropTypes.string.isRequired,
+    //description: PropTypes.string,
+    //type: PropTypes.string,
+    //capacity: PropTypes.number,
+    //price: PropTypes.number.isRequired,
+//}
 
 
 export default ProductsListItem
