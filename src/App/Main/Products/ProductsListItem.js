897906d9a08657ec86c85eb084ede87428ccb037
Yulia <homka15@i.ua>
2018-11-17 13:10:12 +0200

    add increment ti plus 1 to products amount

--- src/App/Main/Products/ProductsListItem.js
+++ src/App/Main/Products/ProductsListItem.js
@@ -20,6 +20,12 @@ class ProductsListItem extends Component {
         productsCount: 1,
     }
 
+    onIncrementClick () {
+        this.setState((prevState)=>({
+            productsCount: prevState.productsCount + 1
+        }))
+    }
+
     render () {
 
         const {
@@ -43,7 +49,7 @@ class ProductsListItem extends Component {
                 <div className="product-quantity">
                     <button>-</button>
                     <input type="text" value={this.state.productsCount} readOnly/>
-                    <button>+</button>
+                    <button onClick={()=>this.onIncrementClick()}>+</button>
                 </div>
                 <div className="product-price">$ {price}</div>
                 <button className="btn-add-to-cart">Add to cart</button>
