b5c0f998b4a5fba59185cb8d08318f94d9f003ed
Yulia <homka15@i.ua>
2018-11-18 12:43:34 +0200

    make App.js a statefull one

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import  Header from './Header/Header'
 import  Main from './Main/Main'
@@ -7,16 +7,18 @@ import '../common/style/reset.css'
 import '../common/style/base.css'
 
 
-
-const App = () => {
-	return (
-		<div className="App">
-			<Header/>
-			<Main/>
-			<Footer/>
-		</div>
-	)
+class App extends Component {
+	render () {
+		return (
+			<div className="App">
+				<Header/>
+				<Main/>
+				<Footer/>
+			</div>
+		)
+	}
 }
+
 
 
 
