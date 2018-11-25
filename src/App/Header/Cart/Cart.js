4e94603a17d1237710f5ab37ea37f927a2c8359e
Yulia <homka15@i.ua>
2018-11-25 14:03:26 +0200

    add method key to fix not unique of elements

--- src/App/Header/Cart/Cart.js
+++ src/App/Header/Cart/Cart.js
@@ -4,14 +4,15 @@ import products from './../../Main/Products/products'
 
 import './cart.css'
 
-	
+	console.log(products)
+
 	const Cart = ({
 		productsInCart,
 	}) => 
 	<div className="cart text-center">
 		{  /*['2','3']*/
 			keys(productsInCart).map((productId)=>(
-				<div>
+				<div key={productId}>
 					<span>{productId}</span>: {productsInCart[productId]}
 				</div>
 			))
