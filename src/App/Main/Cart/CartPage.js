c592b7407ec14ff57b21d151721cd15aeff5d7a7
Yulia <homka15@i.ua>
2018-12-02 13:20:50 +0200

    add methods from Cart to CartPage

--- src/App/Main/Cart/CartPage.js
+++ src/App/Main/Cart/CartPage.js
@@ -1,17 +1,35 @@
 import React from 'react'
+import {keys} from 'lodash'
+import products from './../Products/products'
 
-import Cart from '../../Header/Cart/Cart'
 
 
-const CartPage = () => (
-    <div>
-        <Cart/>
+const productsMap = products.reduce((map,product)=>({
+	...map,
+	[product.id]:product,
+}),{})
+
+const CartPage = ({productsInCart,
+}) => (
+   
+    <div className="cart-page">
+        <h1>Cart Page</h1>
+        {
+		keys(productsInCart).map((productId)=>(
+			<div key={productId}>
+				<span>{productsMap[productId].name}</span>: {productsInCart[productId]}
+			</div>
+		))
+	}
+	<div>
+        Total: $ {
+            keys(productsInCart).reduce((total,product) => {
+                return total + (productsMap[product].price * productsInCart[product])
+            },0)
+        }
+	    </div>
     </div>
 )
 
-    // <div>
-    //     <h1>CartPage</h1>
-    // </div>
-    
 
 export default CartPage
