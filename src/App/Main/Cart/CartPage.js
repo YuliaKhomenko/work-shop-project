a7392f1af7fc838cdb83075f28b17dc041591599
Yulia <homka15@i.ua>
2018-12-02 13:12:02 +0200

    add component Cart to CartPage

--- src/App/Main/Cart/CartPage.js
+++ src/App/Main/Cart/CartPage.js
@@ -4,16 +4,14 @@ import Cart from '../../Header/Cart/Cart'
 
 
 const CartPage = () => (
-    
-		return (
-            <div>
-                <Cart/>
-            </div>
+    <div>
+        <Cart/>
+    </div>
+)
+
     // <div>
     //     <h1>CartPage</h1>
     // </div>
-    )
-    )
-
+    
 
 export default CartPage
